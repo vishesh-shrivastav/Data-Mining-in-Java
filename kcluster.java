613b33508a66a0f622d73ec3c9b9ccfdcbdbff80
Vishesh <vishesh2k6@gmail.com>
2018-10-17 17:57:42 -0500

    Update kcluster.java

--- kcluster.java
+++ kcluster.java
@@ -13,7 +13,8 @@ public class kcluster {
 	static int [] seeds = {1,3,5,7,9,11,13,15,17,19,21,23,25,27,29,31,33,35,37,39};
 	static int trials;
 	static int number_of_clusters;
-	static String filename = "/home/shriv070/K_means/freq.csv";
+	//static String filename = "/home/shriv070/K_means/freq.csv";
+	static String filename = "freq.csv";
 	static String classfile;
 	public static int[][] c_matrix;
 	static HashMap<Integer, ArrayList<Integer>> new_clusters = new HashMap<Integer, ArrayList<Integer>>();
